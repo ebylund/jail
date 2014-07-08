ee5b47aebff4d23c59d94627d82e370e3e1d0a64
Erik Bylund:q <ebylund@ip-144-38-176-218.dixie.edu>
2014-07-08 14:43:14 -0600

    beginnings of the parsing

--- app/controllers/jails_controller.rb
+++ app/controllers/jails_controller.rb
@@ -2,10 +2,19 @@ class JailsController < ApplicationController
 	require 'open-uri'
 	require 'nokogiri'
 	def main
-		url = "https://news.washeriff.net/wp-content/reports/booking/mainbook.html"
+		url = "https://news.washeriff.net/inmate-information/bookings/"
 		doc = Nokogiri::HTML(open(url))
-		@body = doc.css('table')
+		@body = [] #doc.css('.name p').text[/([A-Z]*)/]
+		@table = []
+		@charge = []
 
+		doc.css("table").each do |table|
+			@body << table
+
+			# @body << table.at_css('.name p').text[/([A-Z]*)/]	
+			# @table << table.at_css('.picture img')
+			@charge << table.at_css('.charge')
+		end
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707042015) do
+ActiveRecord::Schema.define(version: 20140708171345) do
 
   create_table "jails", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
