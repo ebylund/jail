00d74dd062071d0455eebe5191de3b310047dd3a
Erik Bylund (Mac Air):q <ebylund@ip-144-38-176-218.dixie.edu>
2014-07-18 08:40:53 -0600

    made angular accessible in the view via factory

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -1,8 +1,22 @@
 (function (){
-	angular.module('jailBirds').controller('BookeePage', ['$scope', 'Bookee', 'Crime', 'ConvictOffense' , function ($scope, Bookee, Crime, ConvictOffense) {
-		$scope.test = "Hello, world";
-		$scope.bookees = Bookee.query();
-		$scope.crimes = Crime.query();
-		$scope.convict_offenses = ConvictOffense.query();
+	angular.module('jailBirds')
+	.controller('BookeePage', ['$scope', 'Bookee', 'Crime', 'ConvictOffense' ,'Data', function ($scope, Bookee, Crime, ConvictOffense, Data) {
+			Data.loadData();
+			$scope.Bookees = Bookees;
+			$scope.Crime = Crime;
+			$scope.ConvictOffense = ConvictOffense;
+
 	}]);
 })();
+
+ng-repeat = 'bookee in Bookee.all()'
+--Bookee data--
+
+
+ng-repeat = 'convict_offense_id in bookee.convict_offense_ids' ng-init="convict_offense = ConvictOffense.find(convict_offense_id)"
+<div ng-init="crime = Crime.find(convict_offense.crime_id)"\
+ +++crime data+++
+ </div>
+
+ <div
+ 	offense data
--- app/assets/javascripts/factory.js
+++ app/assets/javascripts/factory.js
@@ -1,32 +1,75 @@
 (function (){
 	angular.module('jailBirds')
 
-	.factory('Data'), [function($http){
+	.factory('Data', ['$http', function($http){
 		return {
-			"data": {}
-			"load data": function(){}
-		}
-	}]
+			"data": {},
+			"loadData": function(callback){
+				var _this = this;
+				$http.get('/api/bookees.json').success(function(data){
+					_this['data'] = data;
 
-	.factory('Bookee', ['$resource','Data' function($resource, Data){
-		return $resource('/api/bookees/:id', { id: '@id'}, {
-			update: { method: 'PATCH' }
-		});
-	}])
-
-	.factory('Crime', ['$resource', function($resource){
-		return $resource('/api/crimes/:id', { id: '@id'}, {
-			update: { method: 'PATCH' },
-			"find": function(id){
-				Data.data.bookees
+					if(typeof callback == 'function') callback.call(data);
+				})
 			}
-		});
+		}
 	}])
 
-	.factory('ConvictOffense', ['$resource', function($resource){
-		return $resource('/api/convict_offenses/:id', { id: '@id'}, {
-			update: { method: 'PATCH' }
-		});
+	.factory('Bookee', ['Data', function(Data){
+		return {
+			'all': function(){
+				return Data.data.bookees;
+			},
+			"find": function(id){
+				var _ref = this.all()
+				for(var i = 0; i < _ref.length: ++i){
+					var bookee = _ref[i];
+					if(bookee.id == id) return bookee;
+				}
+				return null;
+
+			}
+		}
+
+		
+	}])
+
+	.factory('Crime', ['Data', function(Data){
+
+		return {
+			'all': function(){
+				return Data.data.crimes;
+			},
+			"find": function(id){
+				var _ref = this.all()
+				for(var i = 0; i < _ref.length: ++i){
+					var crime = _ref[i];
+					if(crime.id == id) return crime;
+				}
+				return null;
+
+			}
+		}
+		
+	}])
+
+	.factory('ConvictOffense', ['Data', function(Data){
+		return {
+			'all': function(){
+				return Data.data.convict_offenses;
+			},
+
+			"find": function(id){
+				var _ref = this.all()
+				for(var i = 0; i < _ref.length: ++i){
+					var convict_offense = _ref[i];
+					if(convict_offense.id == id) return convict_offense;
+				}
+				return null;
+
+			}
+		}
+
 	}]);
 
 
