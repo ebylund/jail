e9a5e76c76da8d1164870433aba2fd320283237e
jacobped13 <epedersen@ip-144-38-180-37.dixie.edu>
2014-07-18 09:19:42 -0600

    made style changes

--- db/schema.rb
+++ db/schema.rb
@@ -50,15 +50,4 @@ ActiveRecord::Schema.define(version: 20140718004828) do
     t.datetime "updated_at"
   end
 
-  create_table "jails", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tags", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
